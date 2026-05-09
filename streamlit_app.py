9ed246b637c454102765d342fbfc00455ba42384
Prof. Dr. Poldo Schallowski <leopold.schaller@stud-mail.uni-wuerzburg>
2026-05-09 14:12:32 +0200

    streamlit added

---
+++ streamlit_app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from services.supabase_client import supabase
+from utils.statistic import run_statistics
+from utils.book import run_book
+from utils.login_and_registration import login_and_registration
+from utils.profile import profile_menu
+from utils.home import home
+
+
+if "session" in st.session_state:
+    supabase.auth.set_session(
+        access_token=st.session_state.session.access_token,
+        refresh_token=st.session_state.session.refresh_token
+    )
+
+st.set_page_config(page_title="Schafkopf - Login", page_icon="🔐")
+
+# -------- Session initialisieren --------
+if "user" not in st.session_state:
+    st.session_state.user = None
+
+
+# =========================================
+# NICHT EINGELOGGT → LOGIN / REGISTER
+# =========================================
+if st.session_state.user is None:
+
+    st.title("Hallo liebe Freunde des Schafkopfens")
+
+    login_and_registration()
+# =========================================
+# EINGELOGGT → APP BEREICH
+# =========================================
+else:
+    user = st.session_state.user
+
+    # -----------------------------
+    # SIDEBAR NAVIGATION
+    # -----------------------------
+    st.sidebar.title("Menü")
+    menu = st.sidebar.radio(
+        "Navigation",
+        ["Home", "Profil", "Sheephead-Book", "Sheephead-Statistic", "Logout"]
+    )
+
+    # =====================================
+    # DASHBOARD
+    # =====================================
+    if menu == "Home":
+        home(user)
+
+    # =====================================
+    # PROFIL
+    # =====================================
+    elif menu == "Profil":
+        profile_menu(user)
+
+    # =====================================
+    # SPACE 1
+    # =====================================
+    elif menu == "Sheephead-Book":
+        run_book()
+
+
+    # =====================================
+    # SPACE 2
+    # =====================================
+    elif menu == "Sheephead-Statistic":
+        run_statistics()
+
+    # =====================================
+    # LOGOUT
+    # =====================================
+    elif menu == "Logout":
+        logout = st.button("Logout?")
+        if logout:
+            supabase.auth.sign_out()
+            st.session_state.clear()
+            st.rerun()
+
+
+# streamlit run /Users/leopoldschaller/Desktop/Sheephead/streamlit_app.py
+
+# Daten migrieren
+# Code verschlanken???
+# Spielerstatistik auch nach Spielart und mit komplexen formeln...

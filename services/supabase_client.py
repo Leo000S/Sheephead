a3b44a4945780f6d823983c581c1f634bc4ef251
Prof. Dr. Poldo Schallowski <leopold.schaller@stud-mail.uni-wuerzburg>
2026-05-09 14:25:34 +0200

    secrets changed

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -2,8 +2,6 @@ import streamlit as st
 from supabase import create_client
 
 url = st.secrets["SUPABASE_URL"]
-key = st.secrets["SUPABASE_ANON_KEY"]
-service_key = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
+key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
-supabase_admin = create_client(url, service_key)

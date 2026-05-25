21c4d150e937f52308824a43d287d135edbb450f
Prof. Dr. Poldo Schallowski <leopold.schaller@stud-mail.uni-wuerzburg>
2026-05-25 17:56:56 +0200

    turnier aux 2505 erstellt

--- SpieleAuswahl.py
+++ SpieleAuswahl.py
@@ -1,6 +1,6 @@
 # --- Konfiguration ---
 SPIELWERTE = {"Ruf": 1, "Ramsch": 1, "Trumpfsolo": 5, "Wenz": 5, "Geier": 5, "Bettel": 5, "Durchmarsch": 5}
-SPIELWERTE_AUX = {"Ruf": 1, "Ramsch": 1, "Trumpfsolo": 5, "Wenz": 5, "Durchmarsch": 5}
+SPIELWERTE_AUX = {"Ruf": 1, "Ramsch": 1, "Trumpfsolo": 5, "Wenz": 5, "Durchmarsch": 5, "Bettel": 5}
 SPIELWERTE_AllR = {"Ruf": 1, "Trumpfsolo": 5, "Wenz": 5, "Bettel": 5}
 
 # = Spielarten, Klopfen, Tout, Punkteart
